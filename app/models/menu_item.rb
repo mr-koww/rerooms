b3ad1246df84f96e61c0a38d23e7d46b88fe7e68
mr-koww <mr-koww@yandex.ru>
2022-04-23 14:10:51 +0300

    add state machine to menu item model

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,3 +1,19 @@
 class MenuItem < ApplicationRecord
   validates :name, presence: true
+
+  # State machine
+  include AASM
+
+  aasm column: 'state' do
+    state :new, initial: true
+    state :active, :disabled
+
+    event :activate do
+      transitions from: :new, to: :active
+    end
+
+    event :disable do
+      transitions from: %w[new active], to: :disabled
+    end
+  end
 end
